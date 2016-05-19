2f16b8e87b6061561f7d07dc8187c4bf255c7d08
ilap <pal.dorogi@gmail.com>
2016-05-19 20:34:04 +0100

    Cleaned the UnitTest file.

--- BioSwiftTests/BioSwiftTests.swift
+++ BioSwiftTests/BioSwiftTests.swift
@@ -73,6 +73,34 @@ class BioSwiftTests: XCTestCase {
         self.measureBlock {
             // Put the code you want to measure the time of here.
         }
+
+        /*
+         var start = NSDate()
+         //var seqIO = SeqIO(path: "/Users/ilap/Developer/Dissertation/DesignGuide/Utils/Sequences/Bacillus_subtilis-ATCC6051_whole_genome/sequence.fasta.txt")
+         guard let seqrecords = try SeqIO.parse(parameters.source) else { return }
+
+         //print("SEQRECORD.... \(seqrecords)")
+         let seqRecord = seqrecords[0]!
+
+         var end = NSDate()
+         var timeInterval: Double = end.timeIntervalSinceDate(start)
+
+         print("Timeinterval \(timeInterval)")
+
+         start = NSDate(); print ("GC Content \(seqRecord.gcContent.format(".2"))"); end = NSDate(); timeInterval  = end.timeIntervalSinceDate(start);print("Timeinterval \(timeInterval)")
+         start = NSDate(); print ("GC Content \(seqRecord.bases)"); end = NSDate(); timeInterval  = end.timeIntervalSinceDate(start);print("Timeinterval \(timeInterval)")
+         start = NSDate(); print ("GC Content \(seqRecord.length)"); end = NSDate(); timeInterval  = end.timeIntervalSinceDate(start);print("Timeinterval \(timeInterval)")
+
+         print (seqRecord[15] as String)
+
+         start = NSDate()
+         let hash = seqRecord.seq.sequence.hash
+         print ("HASVALUE: \(String(hash, radix: 16))")
+         end = NSDate()
+         timeInterval  = end.timeIntervalSinceDate(start);print("Timeinterval \(timeInterval)")
+         //print ("Bases \(seqRecord.bases)")
+         //print ("Length \(seqRecord.length)")
+         */
     }
     
 }
