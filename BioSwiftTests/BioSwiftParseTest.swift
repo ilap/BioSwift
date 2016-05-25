b8506b3f2fc0053dd0b9b1a4adfc1cb6d0532666
ilap <pal.dorogi@gmail.com>
2016-05-25 21:39:46 +0100

    Added parser Unit Test.

--- BioSwiftTests/BioSwiftParseTest.swift
+++ BioSwiftTests/BioSwiftParseTest.swift
@@ -1,28 +1,60 @@
-//
-//  BioSwiftParseTest.swift
-//  BioSwift
-//
-//  Created by Pal Dorogi on 24/05/2016.
-//  Copyright © 2016 Pal Dorogi. All rights reserved.
-//
+/*-
+ *
+ * Author:
+ *    Pal Dorogi "ilap" <pal.dorogi@gmail.com>
+ *
+ * Copyright (c) 2016 Pal Dorogi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import XCTest
+@testable import BioSwift
 
-class BioSwiftParseTest: XCTestCase {
+
+class BioSwiftParserTests: XCTestCase {
+    let mainBundle = NSBundle.mainBundle()
+    let testBundle = NSBundle(forClass: BioSwiftParserTests.self)
 
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testParsingCasOffinderOutput() {
+        //let filePath = testBundle.resourcePath
+        let fileName = testBundle.pathForResource("TestParsers/result", ofType: "bwt")
+
+        // FileParserFacade facade = new FileParserFacade();
+        let facade = ScoreParseManager()
+
+        do {
+            try facade.parseFile(fileName!)
+            for result in (facade.parser?.results)! {
+                print("ITEM: \(result.guideRNA)")
+            }
+        } catch let error {
+            print ("BIOSWIFT ERROR:: \(error)")
+        }
+
+
     }
 
     func testPerformanceExample() {
@@ -31,5 +63,6 @@ class BioSwiftParseTest: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-
+    
 }
+
--- BioSwiftTests/BioSwiftTests.swift
+++ BioSwiftTests/BioSwiftTests.swift
@@ -1,10 +1,23 @@
-//
-//  BioSwiftTests.swift
-//  BioSwiftTests
-//
-//  Created by Pal Dorogi on 16/04/2016.
-//  Copyright © 2016 Pal Dorogi. All rights reserved.
-//
+/*-
+ *
+ * Author:
+ *    Pal Dorogi "ilap" <pal.dorogi@gmail.com>
+ *
+ * Copyright (c) 2016 Pal Dorogi
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 import XCTest
 @testable import BioSwift
@@ -67,7 +80,26 @@ class BioSwiftTests: XCTestCase {
         assert(reversePams == ["TNNAAN", "ATTNNT", "GANNGC"])
 
     }
-    
+
+    func testOffTargetsInSequence() {
+        let genome =  Seq(sequence: "AAAAAAAAAAAAAAAAAAAAAAAAAAGTTTTTTTTTTTTTTTTTTTTTTTTTTTTTGGCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCAG")
+           //01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890
+
+        let record = SeqRecord(id: "TestRecord", seq: genome)
+        let onTargetLoci = 50
+        let onTargetLength = 10
+
+        let usedPAMs = ["NGG"]
+        let allPAMs = ["NGG", "NGA", "NAG", "NAA"]
+
+        let crisprUtil = CrisprUtil(record: record, usedPAMs: usedPAMs, allPAMs: allPAMs)
+        crisprUtil.getScoredOfftargets(onTargetLoci, targetLength: onTargetLength)
+
+
+        print("MASKEDlllll PAM: \(crisprUtil.maskedPAM)")
+
+    }
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
